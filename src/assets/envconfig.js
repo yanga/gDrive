c37ad894c094d8cd2d63719c25faebf2e133f0ea
yanga <yangabay@gmail.com>
2021-09-28 21:33:51 +0300

    npm install serve --s

--- src/assets/envconfig.js
+++ src/assets/envconfig.js
@@ -2,6 +2,6 @@
   window["envconfig"] = window["envconfig"] || {};
 
   // Environment variables
-  window["envconfig"]["apiurl"] = "http://localhost:8080/api";
+  window["envconfig"]["apiurl"] = "http://localhost:3000/api";
   console.log('ENV:::', window["envconfig"]);
 })(this);
